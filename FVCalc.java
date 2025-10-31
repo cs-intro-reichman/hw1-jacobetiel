b3a94b4c127141fc45cf0d4649a6fd006e0165dc
Jacob Etiel <jacob.etiel@post.runi.ac.il>
2025-10-31 19:57:38 +0200

    Completed assignment

--- FVCalc.java
+++ FVCalc.java
@@ -5,6 +5,6 @@ public class FVCalc {
         rate = rate/100;
         int years = Integer.parseInt(args[2]); // length in years
         double futureValue = (Math.pow(rate+1, years)*currentValue);
-        System.out.println("After "+years+" years, "+currentValue+"$ saved at "+(rate*100)+"% will yield $"+(int)(futureValue));
+        System.out.println("After "+years+" years, $"+currentValue+" saved at "+(rate*100)+"% will yield $"+(int)(futureValue));
     }
     }
