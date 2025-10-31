b321b5c1b5df85012d801d300e02b9e9f0d15778
Jacob Etiel <jacob.etiel@post.runi.ac.il>
2025-10-31 19:50:54 +0200

    Completed assignment

--- Ascend2.java
+++
@@ -1,7 +0,0 @@
-// Generates three integer random numbers in a given range,
-// and prints them in increasing order.
-public class Ascend2 {
-	public static void main(String[] args) {
-		// Replace this comment with your code
-	}
-}
--- FVCal2.java
+++
@@ -1,6 +0,0 @@
-// Computes the future value of a saving investment.
-public class FVCal2 {
-	public static void main(String[] args){
-		// Replace this comment with your code
-	}
-}
