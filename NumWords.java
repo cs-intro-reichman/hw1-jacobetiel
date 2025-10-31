f1b4b64c5bd9a3f39aebac27e99d71c7138a1165
Jacob Etiel <jacob.etiel@post.runi.ac.il>
2025-10-31 20:01:52 +0200

    Completed assignment

--- NumWords.java
+++ NumWords.java
@@ -4,7 +4,7 @@ public class NumWords {
         int hundreds = num/100;
         int singles = num%10;
         num = num/10;
-        int tens = num/10;
+        int tens = num%10;
         System.out.println(hundreds+" hundreds, "+tens+" tens, and "+singles+" ones.");
        
     }
